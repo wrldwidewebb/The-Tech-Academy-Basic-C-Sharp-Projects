6737c3f29b2f79a4b7ca7754d2ba6fc9b33613a5
Stephen Webb <71806249+wrldwidewebb@users.noreply.github.com>
2021-01-22 17:35:30 -0800

    Add files via upload

---
+++ CarInsuranceUnderwriting/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsuranceUnderwriting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int b = Convert.ToInt16(age);
+            Console.WriteLine("Have you ever had a DUI? True or False");
+            bool trueOrFalse = Console.ReadLine() == "false";
+            Console.WriteLine("How many speeding tickets do you have?");
+            string speedingTickets = Console.ReadLine();
+            int speedingTicket = Convert.ToInt16(speedingTickets);
+            Console.WriteLine("Qualified?");
+            bool qualification = b > 15 && trueOrFalse && speedingTicket <= 3;
+            Console.Write(qualification.ToString());
+            Console.ReadLine();
+
+        }
+    }
+}

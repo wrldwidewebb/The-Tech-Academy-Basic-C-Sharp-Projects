9a9586a7bcf11a5827a43fd18514276805965892
Stephen Webb <71806249+wrldwidewebb@users.noreply.github.com>
2021-01-25 19:41:54 -0800

    Add files via upload

---
+++ StringbuilderDrill/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringbuilderDrill
+{
+    class Program
+    {
+        static void Main()
+        {
+            string firstName = "Ira";
+            string middlename = "Gret";
+            string lastName = "Jumping";
+
+            Console.WriteLine("His name is " + firstName + " " + middlename + " " + lastName);
+            Console.ReadLine();
+
+            firstName = firstName.ToUpper();
+            Console.WriteLine(firstName);
+            Console.ReadLine();
+
+            lastName = lastName.ToUpper();
+            Console.WriteLine(lastName);
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Good");
+            sb.Append("Better");
+            sb.Append("Best");
+            sb.Append("I hope you feel BEST");
+            sb.Append("Fifty yards to the john");
+            sb.Append("By Kenny Makeit and Betty Wont");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}

68c5c021566abb4a379482acca6d4e3f2f7a2edc
Stephen Webb <71806249+wrldwidewebb@users.noreply.github.com>
2021-01-31 13:13:51 -0800

    Add files via upload

---
+++ MethodDrill/MethodOne.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill
+{
+    public class MethodOne
+    {
+
+        public int mathOperation1 (int addNumber)
+        {
+            int total = addNumber + 10;
+            return total;
+        }
+        public int mathOperation2 (int subtractNumber)
+        {
+            int total = subtractNumber - 10;
+            return total;
+        }
+        public int mathOperation3 (int multiplyNumber)
+        {
+            int total = multiplyNumber * 10;
+            return total;
+        }
+    }
+}
---
+++ MethodDrill/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodOne math = new MethodOne();
+            Console.WriteLine("Please enter a number into the math console ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your number plus 10 = " + math.mathOperation1(input)); 
+            Console.WriteLine("Your number entered minus 10 = " + math.mathOperation2(input)); 
+            Console.WriteLine("Your number entered times 10 = " + math.mathOperation3(input));
+            Console.ReadLine();
+        }
+    }
+}

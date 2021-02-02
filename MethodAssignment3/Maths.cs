2ea2b4a88d7cdb49865c6e9cb27359deaef0f49e
Stephen Webb <71806249+wrldwidewebb@users.noreply.github.com>
2021-02-01 17:32:06 -0800

    Add files via upload

---
+++ MethodAssignment3/Maths.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment3
+{
+    public class Maths
+    {
+        public void Divide(int a)
+        {
+            int result = a / 2;
+            Console.WriteLine("Your number divided by 2 equals: " + result);
+            return;
+        }
+
+        public void Divide(out int x, out int y)
+        {
+            Console.WriteLine("Pick a number");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choose another number");
+            y = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static int Print()
+        {
+            int d = 100;
+            return d;
+        }
+    }
+}
---
+++ MethodAssignment3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Maths m = new Maths();
+            Console.WriteLine("Pick a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int a;
+            int b;
+            m.Divide(num1);
+            m.Divide(out a, out b);
+            Maths.Print();
+
+            Console.WriteLine("The first number is " + a);
+            Console.WriteLine("The second number is " + b);
+            Console.WriteLine("The static number is " + Maths.Print());
+
+            Console.ReadLine();
+        }
+    }
+}

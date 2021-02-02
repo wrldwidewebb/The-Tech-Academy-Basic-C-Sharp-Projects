7bc7243becdde1b02446a063d32dfe30b020f6f5
Stephen Webb <71806249+wrldwidewebb@users.noreply.github.com>
2021-02-01 16:42:18 -0800

    Add files via upload

---
+++ MethodAssignment1/MathMethod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment1
+{
+    class MathMethod
+    {
+        public int Sum(int num1)
+        {
+            var Total = num1 + 22;
+            return Total;
+        }
+        public int Sum(decimal num1)
+        {
+            var Total = num1 - 10;
+            return Convert.ToInt32(Total);
+        }
+        public int Sum(string num1)
+        {
+            var Total = Convert.ToInt32(num1) * 2;
+            return Total;
+        }
+    }
+}

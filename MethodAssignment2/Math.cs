73a4b6d45ced00349ae2a72117f1de00e26a4b84
Stephen Webb <71806249+wrldwidewebb@users.noreply.github.com>
2021-02-01 16:55:10 -0800

    Add files via upload

---
+++ MethodAssignment2/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment2
+{
+    public class Math
+    {
+        public int Addition(int a, int b = 5)
+        {
+            int answer = a + b;
+            return answer;
+        } 
+    }
+}
---
+++ MethodAssignment2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math m = new Math();
+
+            Console.WriteLine("Choose number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Select enter to use the default number");
+            string num2 = Console.ReadLine();
+            Console.WriteLine("Adding the first number to the second number");
+            if (num2 == "")
+            {
+                Console.WriteLine("The answer is " + m.Addition(num1));
+            }
+            else
+            {
+                int num3 = Convert.ToInt32(num2);
+                Console.WriteLine("The answer is " + m.Addition(num1, num3));
+            }
+            Console.ReadLine();
+
+        }
+    }
+}

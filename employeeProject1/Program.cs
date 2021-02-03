eb684196382c30d3b9e1bb40c3f6f07b2463d259
Stephen Webb <71806249+wrldwidewebb@users.noreply.github.com>
2021-02-02 20:06:35 -0800

    Add files via upload

---
+++ employeeProject1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeeProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            employee n = new employee();
+            n.FirstName = "Sample";
+            n.LastName = "Student";
+            n.SayName();
+            Console.ReadLine();
+
+        }
+    }
+}

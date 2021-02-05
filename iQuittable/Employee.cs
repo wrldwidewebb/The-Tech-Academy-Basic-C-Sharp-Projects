5288ddff86f22323aadb9f736db3aaa5bdf80eaf
Stephen Webb <71806249+wrldwidewebb@users.noreply.github.com>
2021-02-04 18:52:04 -0800

    Add files via upload

---
+++ iQuittable/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iQuittable
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+        public void Quit()
+        {
+            Console.WriteLine("Press enter to exit.");
+        }
+
+    }
+}
---
+++ iQuittable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iQuittable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee n = new Employee();
+            n.FirstName = "Sample";
+            n.LastName = "Student";
+            n.SayName();
+            IQuittable m = new Employee();
+            m.Quit();
+            Console.ReadLine();
+        }
+    }
+}

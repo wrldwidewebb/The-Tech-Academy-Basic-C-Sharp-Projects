5fbe78a6cda2790c31550afaed630dc451c9974b
Stephen Webb <71806249+wrldwidewebb@users.noreply.github.com>
2021-02-04 19:31:57 -0800

    Add files via upload

---
+++ GenericEmployee/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericEmployee
+{
+    public class Employee<C>
+    {
+        public List<C> things { get; set; }
+
+    }
+}
---
+++ GenericEmployee/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> n = new Employee<string>();
+            n.things = new List<string>()
+            {
+                "Red",
+                "Blue",
+                "Yellow"
+            };
+
+            Employee<int> m = new Employee<int>();
+            m.things = new List<int>()
+            {
+                1,
+                2,
+                3
+            };
+            n.things.ForEach(x => Console.WriteLine(x));
+            m.things.ForEach(z => Console.WriteLine(z));
+            Console.ReadLine();
+
+        }
+    }
+}

9c7c2983e99f838a7de3fc90487e97d299fb12ff
Stephen Webb <71806249+wrldwidewebb@users.noreply.github.com>
2021-02-04 19:03:24 -0800

    Add files via upload

---
+++ OverloadEmployee/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadEmployee
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        public static bool operator ==(Employee A, Employee B)
+        {
+            bool status = false;
+            if (A == B)
+            {
+                status = true;
+
+            }
+            return status;
+        }
+
+        public static bool operator !=(Employee A, Employee B)
+        {
+            bool status = false;
+            if (A != B)
+            {
+                status = false;
+
+            }
+            return status;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ OverloadEmployee/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee n = new Employee();
+            n.ID = 001;
+            Employee m = new Employee();
+            m.ID = 002;
+
+            bool status = n.ID == m.ID;
+
+            Console.WriteLine("Does employee 1 have the same ID as employee 2? " + status);
+            Console.ReadLine();
+        }
+    }
+}

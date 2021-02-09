2159bb8e1e65d1418391ebe74ec19bb627cf6f5b
Stephen Webb <71806249+wrldwidewebb@users.noreply.github.com>
2021-02-08 17:32:42 -0800

    Add files via upload

---
+++ StructDrill/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructDrill
+{
+    class Program
+    {
+        public struct Number
+        {
+        public decimal Amount;
+    }
+        static void Main(string[] args)
+        {
+            Number price; 
+            price.Amount = 12.2m; 
+
+            Console.WriteLine(price.Amount);
+            Console.ReadLine();
+          
+        }
+    }
+}

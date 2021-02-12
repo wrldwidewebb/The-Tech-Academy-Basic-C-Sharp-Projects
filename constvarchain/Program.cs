062c1cdb54fe1dfbb5eff45172914a563bb501d2
Stephen Webb <71806249+wrldwidewebb@users.noreply.github.com>
2021-02-11 18:47:50 -0800

    Add files via upload

--- constvarchain/Program.cs
+++ constvarchain/Program.cs
@@ -12,9 +12,13 @@ namespace constvarchain
         {
             const string casinoName = "Grand Hotel and Casino";
 
-            var year = 2021;
-            
-            Console.WriteLine("Welcome to " + casinoName + " built in " + year + " enjoy your stay. ");
+            var year = 1979;
+
+            Showrooms showroom1 = new Showrooms(23, "Aries");
+            Showrooms showroom2 = new Showrooms("Apollo");
+
+            Console.WriteLine("Welcome to " + casinoName + " built in " + year + ". Our finest showrooms are:  \n" + showroom1.named + " & " + showroom2.named + "\n");
+
             Console.ReadLine();
 
         }
---
+++ constvarchain/Showrooms.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constvarchain
+{
+    class Showrooms
+    {
+        public int number;
+        public string named;
+        public Showrooms() : this(0, "") { }
+        public Showrooms(int number, string named)
+        {
+            this.number = number;
+            this.named = named;
+        }
+        public Showrooms(int number) : this(number, "") { }
+        public Showrooms(string named) : this(0, named) { }
+    }
+}

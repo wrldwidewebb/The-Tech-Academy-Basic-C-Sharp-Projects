1e5ead05981ef1705913f1257d105d965fff3c59
Stephen Webb <71806249+wrldwidewebb@users.noreply.github.com>
2021-02-11 17:12:34 -0800

    Add files via upload

---
+++ constvarchain/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constvarchain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string casinoName = "Grand Hotel and Casino";
+
+            var year = 2021;
+            
+            Console.WriteLine("Welcome to " + casinoName + " built in " + year + " enjoy your stay. ");
+            Console.ReadLine();
+
+        }
+    }
+}

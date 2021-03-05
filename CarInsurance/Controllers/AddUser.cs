0b6b8e36a950201a3569a65571d7da628057ba74
Stephen Webb <71806249+wrldwidewebb@users.noreply.github.com>
2021-03-04 16:53:10 -0800

    Add files via upload

---
+++ CarInsurance/Controllers/AddUser.cs
@@ -0,0 +1,12 @@
+﻿namespace AutoInsurance.Controllers
+{
+    internal class AddUser
+    {
+        internal object LastName;
+        internal object EmailAddress;
+        internal object Quote;
+
+        public object Id { get; internal set; }
+        public object FirstName { get; internal set; }
+    }
+}

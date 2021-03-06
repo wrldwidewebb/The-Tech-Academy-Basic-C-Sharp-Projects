fab86e3b89f74e9009e1460c05a7509680df9661
Stephen Webb <71806249+wrldwidewebb@users.noreply.github.com>
2021-03-06 11:54:24 -0800

    Update to Car Insurance

--- CarInsurance/CarInsurance/CarInsurance/Controllers/InsureeController.cs
+++ CarInsurance/CarInsurance/CarInsurance/Controllers/InsureeController.cs
@@ -46,12 +46,13 @@ namespace CarInsurance.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType")] Insuree insuree)
         {
             if (ModelState.IsValid)
             {
                 db.Insurees.Add(insuree);
                 db.SaveChanges();
+                CalculateQuote(insuree.Id);
                 return RedirectToAction("Index");
             }
 
@@ -78,12 +79,13 @@ namespace CarInsurance.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType")] Insuree insuree)
         {
             if (ModelState.IsValid)
             {
                 db.Entry(insuree).State = EntityState.Modified;
                 db.SaveChanges();
+                CalculateQuote(insuree.Id);
                 return RedirectToAction("Index");
             }
             return View(insuree);
@@ -123,5 +125,71 @@ namespace CarInsurance.Controllers
             }
             base.Dispose(disposing);
         }
+
+        public ActionResult CalculateQuote(int Id)
+        {
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                var insuree = db.Insurees.Find(Id);
+                var dateOfBirth = insuree.DateOfBirth;
+                var carYear = insuree.CarYear;
+                var carModel = insuree.CarModel;
+                var carMake = insuree.CarMake;
+                var speedingTickets = insuree.SpeedingTickets;
+                var dui = insuree.DUI;
+                var coverageType = insuree.CoverageType;
+
+                var quote = 50.0M;
+
+                if (dateOfBirth.Year >= 2003)
+                {
+                    quote = quote + 100.00M;
+                }
+                else if (dateOfBirth.Year <= 2001 && dateOfBirth.Year >= 1996)
+                {
+                    quote = quote + 50.0M;
+                }
+                else if (dateOfBirth.Year < 1996)
+                {
+                    quote = quote + 25.0M;
+                }
+
+                if (carYear < 2000)
+                {
+                    quote = quote + 25.0M;
+                }
+                else if (carYear > 2015)
+                {
+                    quote = quote + 25.0M;
+                }
+
+                if (carMake == "Porsche")
+                {
+                    quote = quote + 25.0M;
+                }
+
+                if (carMake == "Porsche" && carModel == "911 Carrera")
+                {
+                    quote = quote + 25.0M;
+                }
+
+                quote = quote + (speedingTickets * 10.0M);
+
+                if (dui == true)
+                {
+                    quote = quote + (quote / 4.0M);
+                }
+
+                if (coverageType == true)
+                {
+                    quote = quote + (quote / 2.0M);
+                }
+                insuree.Quote = quote;
+                db.SaveChanges();
+
+            }
+            return View("Index");
+        }
+
     }
 }
